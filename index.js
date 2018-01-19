b3373bcd0e5818767034736b484d2d0fca06f854
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-19 06:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ addToCart('smiles');
 console.log(cart);
 console.log(cart.length);
 console.log(total());
-console.log(Object.keys(cart)[0])
+console.log(Object.keys(cart[0]))
 removeFromCart('love');
 console.log(cart);
 removeFromCart('smiles')
