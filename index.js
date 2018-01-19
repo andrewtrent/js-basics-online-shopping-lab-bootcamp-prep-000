01db108449d362510226c917f847d5dca146a1e3
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-19 06:05:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  if (cart.hasOwnProperty(item) === false){
+  if (cart.getOwnPropertyNames(item) === false){
     console.log(`That item is not in your cart.`);
   } else if(Object.keys(cart[item]) < cart.length - 1){
     var newCart = 
